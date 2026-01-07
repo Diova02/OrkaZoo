03e864b77a6b632f597d3553f3a7f89dc5631934
Geovani Costa <geovanicosta730@gmail.com>
2026-01-07 09:08:11 -0300

    2.0.2.1

--- curiosidades.js
+++ curiosidades.js
@@ -4,7 +4,7 @@ export default [
             en: "<strong>Cnidarians</strong> are members of a zoological phylum that includes jellyfish and other simple, radial, and stinging aquatic animals. I bet you didn't know that."
         }, img: "Agua Viva"},
     {   dica: {
-            pt: "A <strong>Ecolocalização</strong> é um sentido biológico usado por morcegos e golfinhos para detectar a posição de objetos através da emissão (e reflexão) de ondas sonoras. São tipo um Demolidor (ou Professor Xavier) da natureza.",
+            pt: "A <strong>Ecolocalização</strong> é um sentido biológico usado por morcegos e golfinhos para detectar a posição de objetos através da emissão (e reflexão) de ondas sonoras. São tipo um Demolidor da natureza.",
             en: "<strong>Echolocation</strong> is a biological sense used by bats and dolphins to detect the position of objects through the emission (and reflection) of sound waves. They are basically the Daredevil of nature."
         }, img: "Golfinho" },
     {   dica: {
